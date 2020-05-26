e9a902af3db4985cc3ee2a325938e7d256b6808d
André Rodrigues <andreluizchr1@gmail.com>
2020-05-26 09:53:19 -0300

    Configurando transação com banco de dados

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,12 +1,30 @@
 package br.com.casadocodigo.loja.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	private String titulo;
 
 	private String descricao;
 
 	private Integer paginas;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getTitulo() {
 		return titulo;
