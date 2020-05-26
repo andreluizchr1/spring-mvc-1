2a57ed2eee3deb6f20cddec3726502e9837be776
André Rodrigues <andreluizchr1@gmail.com>
2020-05-26 11:04:48 -0300

    Testando cadastro

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,6 +20,9 @@ public class Produto {
 	private String descricao;
 
 	private Integer paginas;
+
+	@ElementCollection
+	private List<Preco> precos;
 
 	public Long getId() {
 		return id;
@@ -48,6 +54,14 @@ public class Produto {
 
 	public void setPaginas(Integer paginas) {
 		this.paginas = paginas;
+	}
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
 	}
 
 	@Override
