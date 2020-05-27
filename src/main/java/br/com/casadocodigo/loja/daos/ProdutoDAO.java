439e7f91ba0fbcff080a06cdfea9b1bada633bab
André Rodrigues <andreluizchr1@gmail.com>
2020-05-27 17:07:30 -0300

    Urls amigaveis

--- src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -24,4 +24,11 @@ public class ProdutoDAO {
 	public List<Produto> listar() {
 		return manager.createQuery("Select p from Produto p", Produto.class).getResultList();
 	}
+
+	public Produto find(Integer id) {
+		return manager.createQuery("select distinct(p) from Produto p "
+				+ "join fetch p.precos preco where p.id = :id", Produto.class)
+				.setParameter("id", id)
+				.getSingleResult();
+	}
 }
