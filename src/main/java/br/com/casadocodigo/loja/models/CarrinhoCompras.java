07e636ee00b26b1239c622d8ec0132caa6b88e71
André Rodrigues <andreluizchr1@gmail.com>
2020-05-28 11:49:34 -0300

    Listando os itens

--- src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.models;
 
+import java.math.BigDecimal;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -24,8 +26,17 @@ public class CarrinhoCompras {
 
 		return itens.get(item);
 	}
-	
+
 	public int getQuantidade() {
-		return itens.values().stream().reduce(0, (proximo,acumulador) -> proximo + acumulador);
+		return itens.values().stream().reduce(0, (proximo, acumulador) -> proximo + acumulador);
 	}
+
+	public Collection<CarrinhoItem> getItens() {
+		return itens.keySet();
+	}
+	
+	public BigDecimal getTotal(CarrinhoItem item) {
+		return item.getTotal(getQuantidade(item));
+	}
+
 }
