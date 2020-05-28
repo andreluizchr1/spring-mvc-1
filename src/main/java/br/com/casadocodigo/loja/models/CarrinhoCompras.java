4c0f1b5472897302849bce65fbad80365caa1895
André Rodrigues <andreluizchr1@gmail.com>
2020-05-28 15:58:31 -0300

    Finalizando o código

--- src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.models;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.LinkedHashMap;
@@ -11,15 +12,20 @@ import org.springframework.web.context.WebApplicationContext;
 
 @Component
 @Scope(value = WebApplicationContext.SCOPE_SESSION)
-public class CarrinhoCompras {
+public class CarrinhoCompras implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<CarrinhoItem, Integer>();
 
 	public void add(CarrinhoItem item) {
 		itens.put(item, getQuantidade(item) + 1);
 	}
 
-	private int getQuantidade(CarrinhoItem item) {
+	public Integer getQuantidade(CarrinhoItem item) {
 		if (!itens.containsKey(item)) {
 			itens.put(item, 0);
 		}
@@ -34,9 +40,18 @@ public class CarrinhoCompras {
 	public Collection<CarrinhoItem> getItens() {
 		return itens.keySet();
 	}
-	
+
 	public BigDecimal getTotal(CarrinhoItem item) {
 		return item.getTotal(getQuantidade(item));
+	}
+
+	public BigDecimal getTotal() {
+		BigDecimal total = BigDecimal.ZERO;
+		for (CarrinhoItem item : itens.keySet()) {
+			total = total.add(getTotal(item));
+		}
+
+		return total;
 	}
 
 }
